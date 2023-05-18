7f7f0dad674bc084939efb31225758a36e4068cb
Chamas111 <ahmad.chamas87@gmail.com>
2023-05-18 06:52:32 +0200

    login and register functionality

--- server/index.js
+++ server/index.js
@@ -1,9 +1,61 @@
 const express = require("express");
-
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+require("dotenv").config();
 const app = express();
+app.use(express.json());
+const jwt = require("jsonwebtoken");
+const PORT = process.env.PORT || 9000;
+const User = require("./models/User");
+const bcryptSalt = bcrypt.genSaltSync(10);
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
+mongoose.connect(process.env.MONGO_URL);
 app.get("/test", (req, res) => {
   res.json("test ok");
 });
 
-app.listen(4000);
+/*register */
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(user);
+  } catch (err) {
+    res.status(422).json({ error: err.message });
+  }
+});
+
+/*login */
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userDoc = await User.findOne({ email });
+    if (userDoc) {
+      const passOk = bcrypt.compare(password, userDoc.password);
+      if (passOk) {
+        const token = jwt.sign(
+          { email: userDoc.email, id: userDoc._id },
+          process.env.JWT_SECRET,
+          {},
+          (err, token) => {
+            if (err) throw err;
+            res.cookie("token", token).json("pass ok");
+          }
+        );
+      }
+    } else {
+      res.status(422).json("pass not ok");
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(PORT, () => console.log(`server is runing on port ${PORT}`));
