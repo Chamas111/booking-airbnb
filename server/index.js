dd197404eaf0e9b3d7286b3c012730bec08fea46
Chamas111 <ahmad.chamas87@gmail.com>
2025-09-06 01:27:29 +0200

    edit on Cors code

--- server/index.js
+++ server/index.js
@@ -189,4 +189,6 @@ app.get("/bookings", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
